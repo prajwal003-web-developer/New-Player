e4c437916de5d8d28d690a1b0d43a40914f08694
royhanraii-art <royhanrai@gmail.com>
2026-06-27 22:26:06 +0545

    first commit

---
+++ app/Player.jsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useSearchParams } from "next/navigation";
+import { servers } from "./servers";
+
+export default function Player() {
+  const searchParams = useSearchParams();
+
+  const server = Number(searchParams.get("server") || 0);
+  const id = searchParams.get("id") || "550";
+  const type = searchParams.get("type") || "movie";
+  const season = searchParams.get("season");
+  const episode = searchParams.get("episode");
+
+  const currentServer = servers[server];
+
+  if (!currentServer) {
+    return (
+      <div className="flex h-screen items-center justify-center bg-black text-white">
+        Invalid Server
+      </div>
+    );
+  }
+
+  let url = `${currentServer.url}${type}/${id}`;
+
+  if (type === "tv" && season && episode) {
+    url += `/${season}/${episode}`;
+  }
+
+  if (currentServer.end) {
+    url += currentServer.end;
+  }
+
+  return (
+    <iframe
+      src={url}
+      className="w-full h-full border-0"
+      allow="fullscreen; autoplay"
+      allowFullScreen
+    />
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,39 +1,18 @@
-'use client';
-
-import { useSearchParams } from 'next/navigation';
-import { servers } from './servers';
+import { Suspense } from "react";
+import Player from "./Player";
 
 export default function Page() {
-  const searchParams = useSearchParams();
-
-  const server = searchParams.get('server');
-  const id = searchParams.get('id') || 550;
-
-  const type = searchParams.get('type') || "movie";
-  const season = searchParams.get('season');
-  const episode = searchParams.get('episode');
-
-  
-
- let url = servers[server||0].url
- url = url+type+"/"+id
-
- if(type=='tv'){
-  url = url+`${season}/${episode}`
- }
-
- if(servers[server||0].end){
-  url= url + servers[server||0]?.end
- }
- console.log(url)
   return (
     <div className="h-[100dvh] w-full bg-black">
-      <iframe
-        src={url}
-        className="w-full h-full border-0"
-        allow="fullscreen; autoplay"
-        allowFullScreen
-      />
+      <Suspense
+        fallback={
+          <div className="flex h-full items-center justify-center text-white">
+            Loading Player...
+          </div>
+        }
+      >
+        <Player />
+      </Suspense>
     </div>
   );
 }

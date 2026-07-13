b2dec7d3e87d30807c1e907902e06ef89598b082
prajwal003-web-developer <prajwalneupane003@gmail.com>
2026-07-13 15:43:03 +0545

    first commit

--- app/api/live/route.js
+++ app/api/live/route.js
@@ -3,44 +3,47 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
 
-    const res = await fetch("https://dami-tv.pro/papi/api/streams")
-    const myData = await res.json()
+    const res = await fetch("https://daddylive.eu/api/events")
+    const data = await res.json()
 
-    const res2 = await fetch("https://dami-tv.pro/papi/matches/cricket")
-    const myData2 = await res2.json()
+    const myData = data[0].categories["Popular Live Events"]
 
-
-    const data = myData?.streams?.map((itm)=>{
+    const retData = myData?.map((itm)=>{
+        const sources = itm?.channels?.map((itm)=>{
+            return {
+                name:itm?.channel_name ,
+                embed:itm?.url
+            }
+        })
         return {
-            name:itm?.category,
-            title:itm?.streams[0].name,
-            image:itm?.streams[0]?.poster,
-            teams:itm?.streams[0].teams,
-            status:itm?.streams[0]?.status,
-            league:itm?.streams[0]?.league,
-            sources:itm?.streams[0].sources,
-            embed:itm?.streams[0].uri_name
-
-        }
-    })
-
-    const data2 = myData2?.map((itm)=>{
-        return {
-            name:itm?.category,
+            name:itm?.event,
             title:itm?.title,
-            image:itm?.poster,
-            teams:itm?.teams,
-            status:itm?.status,
-            league:itm?.league,
-            sources:[{
-                 "name": "Server 1",
-                "embed": `https://dami-tv.pro/embed/?id=${itm?.sources[0].id}`
-            }],
-            embed:itm?.sources[0].id
-
+            league:"Unknown Sorry !!",
+            teams:'',
+            status:"live",
+            embed:itm?.channels[0]?.url,
+            sources:sources,
+            image:`https://dummyimage.com/600x400/000/fff&text=${itm?.event}`
         }
     })
-    return NextResponse.json({
-       data:[...data,...data2]
-    })
+
+
+    // const data = myData?.streams?.map((itm)=>{
+    //     return {
+    //         name:itm?.category,
+    //         title:itm?.streams[0].name,
+    //         image:itm?.streams[0]?.poster,
+    //         teams:itm?.streams[0].teams,
+    //         status:itm?.streams[0]?.status,
+    //         league:itm?.streams[0]?.league,
+    //         sources:itm?.streams[0].sources,
+    //         embed:itm?.streams[0].uri_name
+
+    //     }
+    // })
+
+   return NextResponse.json({
+    data:retData
+   })
+   
 }
